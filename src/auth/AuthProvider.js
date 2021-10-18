2d6de4cd2f18b21f72392ecbbb7cae8ff5982be4
Jimmy Murillo <salchichongallo1997@gmail.com>
2021-10-18 09:15:47 -0500

    fix(useUser): avoid destructuring error

--- src/auth/AuthProvider.js
+++ src/auth/AuthProvider.js
@@ -3,7 +3,7 @@ import React, {useState, createContext} from 'react';
 export const AuthContext = createContext();
 
 function AuthProvider({children}) {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState({});
   const onAuth = nextUser => setUser(nextUser);
   return (
     <AuthContext.Provider value={{user, onAuth}}>
