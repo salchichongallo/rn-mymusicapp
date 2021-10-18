879d19d066b4deeab8128434993409aa61c49efe
Jimmy Murillo <salchichongallo1997@gmail.com>
2021-10-18 09:20:51 -0500

    feat: add custom ActivityIndicator

---
+++ src/components/ActivityIndicator.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {ActivityIndicator as BaseActivityIndicator} from 'react-native';
+
+function ActivityIndicator(props) {
+  return <BaseActivityIndicator color="#1eee67" {...props} />;
+}
+
+export default ActivityIndicator;
